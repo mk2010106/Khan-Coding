6c0f8e6ad1ae136a2a662006fc25b5ccd6bdef92
Muhammad Khalid <159458833+mk2010106@users.noreply.github.com>
2024-06-25 15:11:22 +0500

    Data types Basic

---
+++ 02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as a newer version 
+
+ //alert(3+3) "we are using node js not browser so alert window is not working"
+ console.log(3+3); 
+ 
+ console.log("Khalid") // code reability should be high
+//  a website for documentation https://tc39.es/ecma262/ and mdn should be used
+let name= "Khalid" //string 
+let age = 29 // numeric
+let isLoggedIn= false // boolean
+// number => 2 to the power of 52 o53 
+// bigint
+//string i preferr using ""
+// bolean => true or false
+// null standalone value
+// undefined
+// symbol used mostly in React and figma
+//object
+console.log(typeof undefined); //undefined
+
+console.log(typeof null); //object
